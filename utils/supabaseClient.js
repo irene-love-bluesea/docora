8d85e59cb86e04bd4fa12ddf03a3dad268d0d662
ayeminaung010 <ayeminaung.mf@gmail.com>
2025-08-11 17:44:36 +0700

    feat: identity file upload

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,7 +1,4 @@
 import { SUPABASE_PROJECT_URL , SUPABASE_ANON_KEY } from '@env';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = SUPABASE_PROJECT_URL;
-const supabaseAnonKey = SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseURL, supabaseAnonKey);
+export const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY);
